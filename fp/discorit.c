1e7f590ff71191917d9fa7321bf4d24163a823a3
winterbitia <amoes.noland@gmail.com>
2024-06-22 17:48:08 +0700

    Client-side Channel/Room display and Key request handling

--- fp/discorit.c
+++ fp/discorit.c
@@ -8,10 +8,24 @@
 #include <sys/socket.h>
 #include <sys/stat.h>
 
+//=========//
+// GLOBALS //
+//=========//
+
 // Global variables
 #define PORT 8080
 #define MAX_BUFFER 1024
 int server_fd;
+
+// Client info variables
+char username[100];
+char password[100];
+char channel[100]="";
+char room[100]="";
+
+//================//
+// SERVER CONNECT //
+//================//
 
 // Server connect function
 void connect_server() {
@@ -39,18 +53,25 @@ void connect_server() {
     printf("Connected to server\n");
 }
 
+//==================//
+// COMMAND HANDLING //
+//==================//
+
 // Account handler function
 int handle_account(const char *buffer) {
+    // Check if buffer is empty
     if (buffer == NULL) {
         perror("buffer is empty");
         exit(EXIT_FAILURE);
     }
 
+    // Send request
     if (send(server_fd, buffer, strlen(buffer), 0) < 0) {
         perror("send failed");
         exit(EXIT_FAILURE);
     }
 
+    // Receive response
     char response[MAX_BUFFER];
     memset(response, 0, MAX_BUFFER);
     if (recv(server_fd, response, MAX_BUFFER, 0) < 0) {
@@ -58,13 +79,17 @@ int handle_account(const char *buffer) {
         exit(EXIT_FAILURE);
     }
 
+    // Parse response
     char *type = strtok(response, ",");
     char *message = strtok(NULL, ",");
 
+    // Default message received
     if (strcmp(type, "MSG") == 0){
         printf("%s\n", message);
         return 0;
     }
+
+    // Login status sending
     else if (strcmp(type, "LOGIN") == 0){
         printf("%s\n", message);
         return 1;
@@ -73,16 +98,19 @@ int handle_account(const char *buffer) {
 
 // Command handler function
 int handle_command(const char *buffer) {
+    // Check if buffer is empty
     if (buffer == NULL) {
         perror("buffer is empty");
         exit(EXIT_FAILURE);
     }
 
+    // Send request
     if (send(server_fd, buffer, strlen(buffer), 0) < 0) {
         perror("send failed");
         exit(EXIT_FAILURE);
     }
 
+    // Receive response
     char response[MAX_BUFFER];
     memset(response, 0, MAX_BUFFER);
     if (recv(server_fd, response, MAX_BUFFER, 0) < 0) {
@@ -90,26 +118,88 @@ int handle_command(const char *buffer) {
         exit(EXIT_FAILURE);
     }
 
+    // Parse response
     char *type = strtok(response, ",");
     char *message = strtok(NULL, ",");
 
+    // Default message received
     if (strcmp(type, "MSG") == 0){
         printf("%s\n", message);
         return 0;
     }
+
+    // Channel/room variable sending
+    else if (strcmp(type, "CHANNEL") == 0){
+        // Parse channel name
+        char *channel_name = strtok(NULL, ",");
+
+        // Check if parsing is correct
+        if (channel_name == NULL) {
+            perror("channel name is empty");
+            exit(EXIT_FAILURE);
+        }
+
+        // Copy channel name
+        strcpy(channel, channel_name);
+        printf("%s\n", message);
+        return 0;
+    } else if (strcmp(type, "ROOM") == 0){
+        // Parse room name
+        char *room_name = strtok(NULL, ",");
+
+        // Check if parsing is correct
+        if (room_name == NULL) {
+            perror("room name is empty");
+            exit(EXIT_FAILURE);
+        }
+
+        // Copy room name
+        strcpy(room, room_name);
+        printf("%s\n", message);
+        return 0;
+    }
+
+    // Key request
     else if (strcmp(type, "KEY") == 0){
         printf("%s\n", message);
         return 1;
     }
+
+    // Quit message
     else if (strcmp(type, "QUIT") == 0){
         printf("%s\n", message);
         return 2;
     }
 }
 
+//================//
+// KEY REQUESTING //
+//================//
+
+void key_request(char *buffer) {
+    // Ask for key
+    printf("Enter key: ");
+
+    // Get key from user
+    char key[100];
+    memset(key, 0, 100);
+    fgets(key, 100, stdin);
+    key[strcspn(key, "\n")] = '\0';
+
+    // Prepare buffer
+    sprintf(buffer, "KEY %s", key);
+
+    // DEBUGGING
+    printf("Sending: %s\n", buffer);
+
+    // Send to server
+    handle_command(buffer);
+}
+
 //======//
 // MAIN //
 //======//
+
 int main(int argc, char *argv[]) {
     // Validate arguments:
     if (argc < 5) {
@@ -132,9 +222,7 @@ int main(int argc, char *argv[]) {
     // Prepare buffer
     char buffer[MAX_BUFFER];
     memset(buffer, 0, MAX_BUFFER);
-    char username[100];
     strcpy(username, argv[2]);
-    char password[100];
     strcpy(password, argv[4]);
 
     // Register user
@@ -160,47 +248,42 @@ int main(int argc, char *argv[]) {
         printf("Sending: %s\n", buffer);
 
         // Login check
-        if (handle_account(buffer) == 1) {
-            // Prepare channel and room variables
-            char channel[100]="";
-            char room[100]="";
+        if (handle_account(buffer) == 1)
+        while(1){
+            // Print user prompt
+            if (strlen(room) > 0) 
+                printf("[%s/%s/%s] ", username, channel, room);
+            else if (strlen(channel) > 0) 
+                printf("[%s/%s] ", username, channel);
+            else 
+                printf("[%s] ", username);
+            
 
-            while(1){
-                // Print user prompt
-                if (strlen(room) > 0) 
-                    printf("[%s/%s/%s] ", username, channel, room);
-                else if (strlen(channel) > 0) 
-                    printf("[%s/%s] ", username, channel);
-                else 
-                    printf("[%s] ", username);
-                
+            // Get user input
+            memset(buffer, 0, MAX_BUFFER);
+            fgets(buffer, MAX_BUFFER, stdin);
+            buffer[strcspn(buffer, "\n")] = '\0';
 
-                // Get user input
-                memset(buffer, 0, MAX_BUFFER);
-                fgets(buffer, MAX_BUFFER, stdin);
-                buffer[strcspn(buffer, "\n")] = '\0';
-
-                // Force client exit command
-                if (strcmp(buffer, "FORCEQUIT") == 0) {
-                    printf("Force quitting client...\n");
-                    close(server_fd);
-                    return 0;
-                }
-
-                // DEBUGGING
-                printf("Sending: %s\n", buffer);
-
-                // Handle command branching
-                int res = handle_command(buffer);
-
-                // Check if user wants to quit
-                if (res == 2)
-                    return 0;
-
-                // Check if user wants to join a channel
-                if (res == 1)
-                    printf("imagine me asking for a key send\n");
+            // Force client exit command
+            if (strcmp(buffer, "FQUIT") == 0) {
+                printf("Force quitting client...\n");
+                close(server_fd);
+                return 0;
             }
+
+            // DEBUGGING
+            printf("Sending: %s\n", buffer);
+
+            // Handle command branching
+            int res = handle_command(buffer);
+
+            // Check if user wants to quit
+            if (res == 2)
+                return 0;
+
+            // Check if user wants to join a channel
+            if (res == 1)
+                key_request(buffer);
         }
     }
 }
